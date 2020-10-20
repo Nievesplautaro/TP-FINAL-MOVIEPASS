6e43651c1a054d2c599a6eba6373aa408d1c8608
Lautaro <nievesp.lautaro@hotmail.com>
2020-10-20 18:26:46 -0300

    Comentado funciones sin implementar

--- Views/navAdmin.php
+++ Views/navAdmin.php
@@ -4,15 +4,15 @@
       <li class="nav-item active">
         <a class="nav-link" href="<?php echo FRONT_ROOT ?>Cinema/ShowMenuView">Cine</a>
       </li>
-      <li class="nav-item">
+<!--       <li class="nav-item">
         <a class="nav-link" href="RUTA_FUNCIONES_ADMNI">Admin Managment</a>
-      </li>
+      </li> -->
       <li class="nav-item">
         <a class="nav-link" href="<?php echo FRONT_ROOT ?>Admin/registerAdmin">Add Admin</a>
       </li>
-      <li class="nav-item">
+<!--       <li class="nav-item">
         <a class="nav-link" href="reservation.php">Settings</a>
-      </li>
+      </li> -->
       <li class="nav-item">
         <a class="nav-link" href="<?php echo FRONT_ROOT ?>Admin/logout">Logout</a>
       </li>
