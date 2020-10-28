3c22170347c2a70c66f4ba8060f97b9a8ac4c887
Lautaro <nievesp.lautaro@hotmail.com>
2020-10-28 19:32:10 -0300

    User nav

--- Views/nav.php
+++ Views/nav.php
@@ -2,14 +2,15 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="<?php echo FRONT_ROOT ?>User/ShowMenuView">Menu</a> 
+        <a class="nav-link" href="<?php echo FRONT_ROOT ?>User/ShowMenuView">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="<?php echo FRONT_ROOT?>Dashboard/showMovies">Dashboard</a> <!-- Showmovies trae todas las peliculas a una lista y las muestra en Views/dashboard -->
+        <a class="nav-link" >Movies</a> <!-- Showmovies trae todas las peliculas a una lista y las muestra en Views/dashboard -->
+        <ul>
+          <li><a href="<?php echo FRONT_ROOT?>Dashboard/showMovies">Show Movies</a></li>
+          <li><a href="<?php echo FRONT_ROOT?>Dashboard/showMovies">My tickets</a></li>
+        </ul>
       </li>
-<!--       <li class="nav-item">
-        <a class="nav-link" href="reservation.php">Reserva</a>
-      </li> -->
       <li class="nav-item">
         <a class="nav-link" href="<?php echo FRONT_ROOT ?>User/logout">Logout</a>
       </li>
