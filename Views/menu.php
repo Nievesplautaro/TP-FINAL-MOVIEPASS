58ad3b9430a11c195b7ad1ac639c5cf5d045198a
Leandro Toloza <leandrotoloza8@gmail.com>
2020-11-03 12:28:06 -0300

    Best Front End Ever (Visual Image Display on Menu)

--- Views/menu.php
+++ Views/menu.php
@@ -2,10 +2,12 @@
      require_once(VIEWS_PATH."nav.php");
 ?>
 
-
-
-
-
-
+     <div id= "ImageAutoDisplay" class="shadow">
+          
+               <img src="<?php echo IMG_PATH ?>OurCinema.jpg" alt="Our Cinema">
+               <img src="<?php echo IMG_PATH ?>TicketDiscount.jpg" alt="Ticket Disccounts">
+               <img src="<?php echo IMG_PATH ?>logo.png" alt="Logo">
+               <img src="<?php echo IMG_PATH ?>favicon.png" alt="Logo">        
+     </div>
 <?php
 ?>
